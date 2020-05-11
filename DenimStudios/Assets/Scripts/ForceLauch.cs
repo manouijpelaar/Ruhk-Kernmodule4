7f0b6b3fa4dcbed87d6c60e5616b18c98f6bdd3e
manouijpelaar <manou.ijpelaar@gmail.com>
2020-05-11 17:34:52 +0200

    tried to fix the seesaw hassel, but it did not work out well

--- DenimStudios/Assets/Scripts/ForceLauch.cs
+++ DenimStudios/Assets/Scripts/ForceLauch.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 public class ForceLauch : MonoBehaviour
 {
     public Rigidbody rb;
+    public Animator launchAnm;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        launchAnm.SetBool("launch", false);
     }
 
     void OnTriggerEnter(Collider launch)
     {   // when player collides with winscene, level complete
         if (launch.gameObject.tag == "LauchBox" && GameObject.FindWithTag("Approval").GetComponent<LauchApproval>().ready == true)
         {
-            rb.AddForce(transform.up * 700 + transform.forward * 260);
-
+            //rb.AddForce(transform.up * 700 + transform.forward * 260);
+            launchAnm.SetBool("launch", true);
 
         }
 
